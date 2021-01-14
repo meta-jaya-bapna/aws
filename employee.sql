1f8606ec6dc1e00312f5e88bf68150db942dc37e
P0130JB <jaya.bapna@email.com>
2021-01-14 20:23:24 +0530

    Created employee table

---
+++ employee.sql
@@ -0,0 +1,10 @@
+Create free mysql database and then create employee table having id, firstName, firstName as fields
+
+CREATE database mysql;
+USE mysql;
+create table employee (
+  id INT UNSIGNED AUTO_INCREMENT,
+  firstName VARCHAR(30),
+  firstName VARCHAR(30),
+  PRIMARY KEY(id));
+ 
